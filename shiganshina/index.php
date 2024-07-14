e5e8ffbbe03fd6cf90602b130ce119002f5ab1e7
elomarii <i.ibrahim.elomari@gmail.com>
2024-07-14 16:05:14 +0200

    basic shiganshina page

---
+++ shiganshina/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to shiganshina</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/07/attack-on-titan-unreal-engine-5-shiganshina.jpg" style="width: 100%"/>
+    <ul>
+	    <li><a href="walls"> The walls</a></li>
+	    <li><a href="scout"> Survey Corpse</a></li>
+	    <li><a href="police"> Military Police</a></li>
+	    <li><a href="garrison"> Garrison Regiment</a></li>
+  </body>
+</html>
