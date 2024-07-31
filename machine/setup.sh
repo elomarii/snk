0444562fcf389e59eea45ffa7f4d95a5c290c658
elomarii <i.ibrahim.elomari@gmail.com>
2024-07-31 14:16:09 +0200

    updates

--- machine/setup.sh
+++ machine/setup.sh
@@ -9,31 +9,28 @@ useradd m.zacharias --create-home
 useradd e.smith --create-home
 
 # POPOLUTE HOME FOLDERS
-cp -r a.arlert /home/a.arlert
-cp -r h.zoe /home/h.zoe
-cp -r m.zacharias /home/m.zacharias
-cp -r e.smith /home/e.smith
+cp -r a.arlert/* /home/a.arlert
+cp -r h.zoe/* /home/h.zoe
+cp -r m.zacharias/* /home/m.zacharias
+cp -r e.smith/* /home/e.smith
 
 # SETUP PASSWORDS
 chpasswd < passwords.txt
 
-# DISABLE COMMANDS HISTORY
-ln -sf /dev/null /home/*/.bash_history
-
 # CONFIGURE PERMISSIONS
-chmod 755 /home/h.zoe
+chown h.zoe:h.zoe /home/h.zoe/* && chmod 755 /home/h.zoe
 chmod +s /home/h.zoe/titans
 chmod 700 /home/h.zoe/scouts_share
-chmod 700 /home/e.smith
-chmod 700 /home/a.arlert
-chmod 700 /home/m.zacharias
+chown a.arlert:a.arlert /home/a.arlert/* && chmod 700 /home/a.arlert
+chown e.smith:e.smith /home/e.smith/* && chmod 700 /home/e.smith
+chown m.zacharias:m.zacharias /home/m.zacharias/* && chmod 700 /home/m.zacharias
 
 # ENABLE NFS SHARE
-apt install nfs-kernel-server
+apt install nfs-kernel-server -y
 systemctl enable --now nfs-server
 echo "/home/h.zoe/research 0.0.0.0/0(rw,sync,no_subtree_check,no_root_squash)" > /etc/exports
 exportfs -arv
 
 # START WEB SERVICES
-apt install docker.io -y
+apt install docker.io docker-compose -y
 cd ../web && docker-compose up --build
